db93f3c41e33a3d2b636cc04edf3e0e74be11e05
paulgreenwood67 <109767559+paulgreenwood67@users.noreply.github.com>
2023-03-02 12:40:56 +0000

    Update server.js

--- server.js
+++ server.js
@@ -54,5 +54,5 @@ if(process.env.NODE_ENV === "production"){
 
 
 app.listen(PORT, () => {
-  console.log(`Listening on port http://localhost/${PORT}`);
+  console.log("Listening on port" + PORT);
 });
